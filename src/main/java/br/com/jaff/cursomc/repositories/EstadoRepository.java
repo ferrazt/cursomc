14418b109079fc92f2319d75a93247beb96cdb23
Jhoel Fiorese <jhoelfiorese@gmail.com>
2021-04-14 14:33:05 -0300

    Cidade e Estado

---
+++ src/main/java/br/com/jaff/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package br.com.jaff.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.jaff.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
